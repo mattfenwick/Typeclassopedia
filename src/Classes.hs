2095ec5524ab4747bc5f6f9d8621432f6ea41683
Matt Fenwick <mfenwick100@gmail.com>
2012-10-25 23:15:15 -0400

    lots of classes, instances, examples

--- src/Classes.hs
+++ src/Classes.hs
@@ -1,8 +1,38 @@
 module Classes (
 
+    Functor'
+  , fmap
+
+  , Pointed'
+  , pure
+
+  , Applicative'
+  , (<*>)
+
+  , Alternative'
+  , empty
+  , (<|>)
+  , guard
+
+  , Alt'
+  , fempty
+  , (<<>>)
+
+  , Monoid'
+  , mempty
+  , mappend
+  , mconcat
+
+  , Monad'
+  , join
+  , (>>=)
+  , (>>)
+
 ) where
 
 import Prelude hiding (fmap, (>>=), (>>), fail)
+
+
 
 
 class Functor' f where
@@ -17,19 +47,62 @@ class Pointed' f where
 class Comonad' m where
 -}
 
+
+class Monoid' a where
+  mempty  :: a
+  mappend :: a -> a -> a
+
   
 class (Functor' f) => Applicative' f where
   (<*>) :: f (a -> b) -> f a -> f b
+
+
+-- why do we need the Applicative' constraint?
+-- why not just: (<|>) :: a -> a -> a ??
+class (Applicative' f) => Alternative' f where
+  empty :: f a
+  (<|>) :: f a -> f a -> f a
+
+
+class Alt' a where
+  fempty :: a
+  (<<>>) :: a -> a -> a
   
   
 class (Applicative' m) => Monad' m where
-  (>>=) :: m a -> (a -> m b) -> m b
+  join :: m (m a) -> m a
+
+
+-- wait ... if it's a Monad, 
+--   and it's an alternative, then
+--   isn't it automatically a MonadPlus?
+-- i.e. (Alternative' m, Monad' m) => ...
+class (Monad' m) => MonadPlus' m where
+  mzero :: m a
+  mplus :: m a -> m a -> m a
   
   
+-- -------------------------------
+-- some more combinators
+
+mconcat :: Monoid' a => [a] -> a
+mconcat = foldr mappend mempty
+
+
+(>>=) :: Monad' m => m a -> (a -> m b) -> m b  
+m >>= f = join (fmap f m)
+
+
+(>>) :: Monad' m => m a -> m b -> m b
+m >> f = m >>= const f
+
+
+guard :: (Pointed' m, Alternative' m) => Bool -> m ()
+guard True = pure ()
+guard False = empty
   
   
-  
--- ---------------
+-- -------------------------------
 -- some instances
 
 instance Functor' [] where
@@ -39,15 +112,43 @@ instance Functor' [] where
   
 instance Pointed' [] where
   pure x = [x]
+
+
+instance Monoid' [a] where
+  mempty = []
+  mappend = (++)
   
   
 instance Applicative' [] where
   fs <*> xs = concat $ fmap (\f -> fmap f xs) fs
 
-  
+
+instance Alternative' [] where
+  empty = []
+  (<|>) = (++)
+
+
+instance Alt' [a] where
+  fempty = []
+  (<<>>) = (++)
+
+
 instance Monad' [] where
---  m a -> (a -> m b) -> m b
-  q >>= f = concat $ fmap f q
+  join = concat
+
+
+instance MonadPlus' [] where
+  mzero = []
+  mplus = (++)
+
+
+
+instance Alt' Bool where
+  fempty = False
+  False <<>> x = x
+  x <<>> _     = x
+  
+
   
   
 instance Functor' IO where
@@ -63,4 +164,46 @@ instance Applicative' IO where
   
   
 instance Monad' IO where
-  q >>= f = do {q' <- q; f q'}
+  join m = do {x <- m; y <- x; return y}
+
+
+
+instance Functor' Maybe where
+  fmap _ Nothing = Nothing
+  fmap f (Just x) = Just (f x)
+
+
+instance Monoid' a => Monoid' (Maybe a) where
+  mempty = Nothing
+  Nothing `mappend` b = b
+  a `mappend` Nothing = a
+  (Just a) `mappend` (Just b) = Just (a `mappend` b)
+
+
+instance Pointed' Maybe where
+  pure = Just
+
+
+instance Applicative' Maybe where
+  Nothing  <*>    _     = Nothing
+  _        <*> Nothing  = Nothing
+  (Just f) <*> (Just x) = Just (f x)
+
+
+instance Alternative' Maybe where
+  empty = Nothing
+  Nothing <|> x = x
+  x <|> _       = x
+
+
+instance Monad' Maybe where
+  join (Just (Just x))  = Just x
+  join      _           = Nothing
+
+
+
+
+
+
+
+
---
+++ src/examples.hs
@@ -0,0 +1,35 @@
+import Classes
+
+import Prelude hiding (fmap, (>>=), (>>), fail)
+
+-- let q s = mplus (digit s) (lower s)
+-- [1..5] >>= \x -> guard (x > 3) >> pure x
+-- Just 0.5 >>= \x -> guard (x > 3) >> pure x
+
+f :: (Monad' m, Monoid' (m [Int]), Pointed' m) => m Int -> m [Int]
+f m = m >>= \x -> if x < 5 
+                  then pure [x - 1 .. x + 1] 
+                  else mempty
+
+
+digit :: String -> Maybe Char
+digit []      = Nothing
+digit (c:_)
+  | elem c ['0'..'9'] = Just c
+  | otherwise         = Nothing
+
+lower :: String -> Maybe Char
+lower [] = Nothing
+lower (c:_)
+  | elem c ['a'..'z'] = Just c
+  | otherwise = Nothing
+ 
+
+
+{-
+-- | Consume a binary character in the input (i.e. either a 0 or an 1)
+binChar :: String -> Maybe Int
+binChar s = digit 0 s `mplus` digit 1 s
+-}
+
+
