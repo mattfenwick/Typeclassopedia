e379b0d8cea0ad547e08d847c46b5c124dafe4e4
Matt Fenwick <mfenwick100@gmail.com>
2012-10-29 13:20:50 -0400

    work on parser instances -- stuck on Applicatives w/o Monads

--- src/examples.hs
+++ src/examples.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE UndecidableInstances #-} -- for P's Semigroup
 import Classes
 
 import Prelude hiding (fmap, (>>=), (>>), fail)
@@ -45,36 +45,121 @@ p2 ('9':cs) = Just ('9', cs)
 p2 _ = Nothing
 
 
-newtype Parser s a = Parser {p :: ([s] -> Maybe (a, [s]))}
+newtype Parser s a = Parser {p :: [s] -> Maybe ([s], a)}
 
-
-gmap :: (a -> b) -> Maybe a -> Maybe b
-gmap = fmap
+newtype NParser s a = NParser {getParser :: [s] -> [([s], a)]}
 
 
 instance Functor' (Parser s) where
-  fmap f (Parser g) = Parser (gmap (\(x, y) -> (f x, y)) . g)
-  
+  -- left 'fmap' is for Parsers
+  -- right 'fmap' 1 is for Maybe
+  -- right 'fmap' 2 is for (,)
+  fmap f (Parser g) = Parser (fmap (fmap f) . g)
+
   
 instance Pointed' (Parser s) where
-  pure x = Parser (\s -> Just (x, s))
+  pure x = Parser (\s -> Just (s, x))
+
   
+{- -}
   
 instance Applicative' (Parser s) where
--- Parser s (a -> b) ----> :: [s] -> Maybe (a -> b, [s])
--- Parser s a        ----> :: [s] -> Maybe (a, [s])
--- Parser s b        ----> :: [s] -> Maybe (b, [s])
+-- Parser s (a -> b) ----> :: [s] -> Maybe ([s], a -> b)
+-- Parser s a        ----> :: [s] -> Maybe ([s], a)
+-- Parser s b        ----> :: [s] -> Maybe ([s], b)
   Parser f <*> Parser g = Parser h
+--    where h xs = fmap (\(ys, f') -> fmap (fmap (f' $)) (g ys)) (f xs)
+--    () <*> g
     where h xs = case (f xs) of
-                      Just (f', ys) -> case (g ys) of
-                                            Just (g', zs) -> Just (f' g', zs);
-                                            Nothing -> Nothing;
+                      -- fmap 1: for Maybe
+                      -- fmap 2: for tuple
+                      Just (ys, f') -> fmap (fmap (f' $)) (g ys)
                       Nothing -> Nothing
-                      
+{-  Parser f <*> Parser g = Parser h
+    where
+      h xs = 
+ -}
+ 
+instance Monad' (Parser s) where
+  join (Parser f) = Parser (\xs -> f xs >>= \(o, Parser g) -> g o)
+  
+  
+instance Functor' (NParser s) where
+  fmap f (NParser g) = NParser (fmap (fmap f) . g)
+  
+instance Pointed' (NParser s) where
+  pure x = NParser (\s -> [(s, x)])
+  
+instance Applicative' (NParser s) where
+  -- NParser s (a -> b)  ----> :: [s] -> [([s], a -> b)]
+  -- NParser s a         ----> :: [s] -> [([s], a)]
+  -- NParser s b         ----> :: [s] -> [([s], b)]
+--  NParser f <*> NParser g = NParser h
+--    where h xs = concatMap (\(ys, f') -> map (fmap (f' $)) (g ys)) (f xs)
+--  f <*> v = hmm f v
+  NParser f <*> NParser g = NParser h
+    where h xs = join (fmap (\(ys, f') -> fmap (fmap (f' $)) (g ys)) (f xs))
+  
+instance Monad' (NParser s) where
+  -- m (m a) -> m a
+  -- NParser s (NParser s a) -> NParser s a
+  -- NParser s (NParser s a)  ----> :: [s] -> [([s], [s] -> [([s], a)])]
+  join (NParser f) = NParser h
+    where
+      h xs = f xs >>= \(out1, NParser g) -> g out1
+    
+    
+hmm :: (Monad' m) => m (a -> b) -> m a -> m b
+hmm mf mv = mf >>= \f -> mv >>= \x -> pure (f x)
+
+ 
+q = Parser (\ss -> case ss of (x:xs) -> Just (xs, x); _ -> Nothing)
+eg1 = ((p q) [1..5], (p q) [])
+eg2 = (p (pure (,) <*> q <*> q)) [1..5]
+eg3 = (p (pure (,) <*> q <*> q)) [1]
+
+dig :: Int -> Parser Int Int
+dig x = Parser (\ss -> case ss of (y:ys) -> if x == y then Just (ys, x) else Nothing; _ -> Nothing)
+
+four = dig 4
+five = dig 5
+
                       
 instance Semigroup' (Parser s a) where
   Parser f <|> Parser g = Parser h
     where h xs = case (f xs) of
                       Just (o, ys) -> Just (o, ys);
                       Nothing      -> g xs
+                      
+instance Monoid' (Parser s a) where
+  empty = Parser (const Nothing)
+
+
+
+
+newtype P m s a = P { getP :: [s] -> m ([s], a) }
+
+
+instance Functor' m => Functor' (P m s) where
+  fmap f (P g) = P (fmap (fmap f) . g)
+
+instance Monad' m => Applicative' (P m s) where
+  P f <*> P x = P h
+    where
+      h xs = f xs >>= \(ys, f') -> 
+        x ys >>= \(zs, x') ->
+        pure (zs, f' x')
+        
+instance Pointed' m => Pointed' (P m s) where
+  pure a = P (\xs -> pure (xs, a))
   
+instance Monad' m => Monad' (P m s) where
+  join (P f) = P h
+    where
+      h xs = f xs >>= \(o, P g) -> g o  
+  
+instance Semigroup' (m ([s], a)) => Semigroup' (P m s a) where
+  P f <|> P g = P (\xs -> f xs <|> g xs)
+  
+instance Monoid' (m ([s], a)) => Monoid' (P m s a) where
+  empty = P (const empty)
